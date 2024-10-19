478b0fd1f4d4e5e84c19fdc67ec6e3d580bbce85
Wenlong Chen <wenlong.chen@getjerry.com>
2024-10-20 00:29:44 +0800

    move

--- src/app/domain/auth/constants.ts
+++
@@ -1,6 +0,0 @@
-export const jwtConstants = {
-  secret:
-    'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.',
-};
-
-export const AUTH_COOKIE_NAME = 'auth';
--- src/app/domain/cat/cat.const.ts
+++
@@ -1,1 +0,0 @@
-export const CAT_QUEUE = 'new-cat-queue';
